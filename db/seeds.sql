6aeb29b33a85914e5382e2b378ffc46cbe8e7b42
Ashley Montine Sproul <montinesproul@gmail.com>
2023-03-30 20:55:34 -0400

    added files started sql

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO employees (first_name, last_name, job_title, department, salary)
+VALUES
+('Ronald', 'Fairbank', 'Mangager', 'Sales', 125 ),
+('Virginia', 'Wolfe', 'Team Lead', 'Sales', 100),
+('Piers', 'Gaveston', 'Sale Person', 'Sales', 95),
+('Charles', 'LeRoi', 'Sr Software Engineer', 'Tech', 100),
+('Katherine', 'Smith', 'Sr Software Engineer', 'Tech', 100 ),
+('Montague', 'Lawrence', 'Jr Software Engineer', 'Tech', 65 ),
+('Octavia', 'Spencer', 'Jr Software Engineer', 'Tech', 65 ),
+('Edward', 'Cullen', 'Social Media', 'Advertising', 70 ),
+('Alice', 'Johnson', 'Customer Service', 'Inbound Sales', 55),
+('Lary', 'Power', 'Receptionist', 'All', 45);
